794577a4e609d9ba2ed50d3155bb35498626b6d4
MachaPadmini <machapdmn30798@gmail.com>
2024-12-12 23:55:40 -0500

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import EventsList from './components/EventsList.vue';
+import MyHeader from './components/MyHeader.vue';
+
+export default{
+  name: "App",
+  components:{
+    EventsList, MyHeader
+  },
+  data(){
+    return {
+      eventlist: []
+    }
+  },
+  methods:{
+    //promoises
+    async fetchevents(){
+      const res = await fetch('https://backend-5nv6.onrender.com/api ');
+      const data = await res.json()
+      console.log(data)
+      return data
+    }
+  },
+  async created(){
+      this.eventlist = await this.fetchevents();
+  }
+}
+</script>
+
+<template>
+  <MyHeader/>
+  <pre>{{eventlist.events}}</pre>
+  <div v-for="event in eventlist">
+    <EventsList :events="event"/>
+  </div>
+  
+</template>
+
+<style></style>
---
+++ frontend/src/components/MyHeader.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Macha's Event Management</h1>
+</template>
+<script>
+export default{
+    name: "MyHeader"
+}
+</script>
+<style>
+</style>
